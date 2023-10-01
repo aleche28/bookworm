8c02a79ed33ad56da4968abc77df041f0df925e1
Alessio Chessa <ale.chessa28@gmail.com>
2023-10-01 21:15:18 +0200

    fixed previous commit

---
+++ src/books.js
@@ -0,0 +1,20 @@
+import { db } from "./firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+async function getReadList(uid) {
+  const docRef = doc(db, "read_books", uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const list = [];
+    // console.log("Document data:", docSnap.data());
+    docSnap.data().list.forEach(b => list.push(b));
+    return list;
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+    return [];
+  }
+}
+
+export { getReadList };
