f63d62199ae0b849b4b45a095de5a74e738bbb4e
Alessio Chessa <ale.chessa28@gmail.com>
2023-10-16 23:06:32 +0200

    added dropdown menu to book row, added function to move book to another list

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../auth/AuthContext";
 import { getList, updateList } from "../books";
-import { Alert, Button, Col, Container, Form, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import { Alert, Button, Col, Container, Dropdown, Form, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 
 
@@ -63,7 +63,7 @@ const BookList = (props) => {
     <>
       <h2>{props.listName}</h2>
       <Col xs={6}>
-        {books.map((b, i) => <BookRow key={i} id={i} book={b} handleDelete={handleDelete}/>)}
+        {books.map((b, i) => <BookRow key={i} id={i} book={b} handleDelete={handleDelete} list={props.listName}/>)}
         {addBook ?
           <Container className="book-add-form my-3 py-3 px-5 border rounded">
             <AddBookForm handleAdd={handleAdd} setAddBook={setAddBook}/>
@@ -126,6 +126,8 @@ const AddBookForm = (props) => {
 }
 
 const BookRow = (props) => {
+  const { user } = useContext(AuthContext);
+
   const tooltip = (sentence) => {
     return (
     <Tooltip id="tooltip">
@@ -134,17 +136,27 @@ const BookRow = (props) => {
     );
   };
 
+  const moveToList = async (destList) => {
+    const list = await getList(user.uid, destList);
+    await updateList(user.uid, destList, [...list, props.book])
+    
+    props.handleDelete(props.id);
+  }
+
   return <>
     <Row className="book-row my-3 py-3 ps-5 pe-5 border rounded">
       <Col xs={4}>{props.book.title}</Col>
       <Col xs={4}>{props.book.author}</Col>
-      <Col xs={1} className="book-row-btn pl-1">
-        <OverlayTrigger placement="top" overlay={tooltip("Mark as read")}>
-          <Button aria-label="Mark as read" variant="success" onClick={() => {}}>
-            <i className="bi bi-check-lg"></i>
-          </Button>
-        </OverlayTrigger>
-      </Col>
+      
+      {props.list !== "Read" &&
+        <Col xs={1} className="book-row-btn pl-1">
+          <OverlayTrigger placement="top" overlay={tooltip("Mark as read")}>
+            <Button aria-label="Mark as read" variant="success" onClick={() => { moveToList("read_books") }}>
+              <i className="bi bi-check-lg"></i>
+            </Button>
+          </OverlayTrigger>
+        </Col>}
+
       <Col xs={1} className="book-row-btn pl-1">
         <OverlayTrigger placement="top" overlay={tooltip("Edit book")}>
           <Button aria-label="Edit book" onClick={() => {}}>
@@ -160,9 +172,20 @@ const BookRow = (props) => {
         </OverlayTrigger>
       </Col>
       <Col xs={1} className="book-row-btn">
-        <Button variant="outline">
-          <i className="bi bi-three-dots-vertical"></i>
-        </Button>
+        <Dropdown>
+          <Dropdown.Toggle variant="outline" id="dropdown-more">
+            <i className="bi bi-three-dots-vertical"></i>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {/* {props.list !== "Read" &&
+              <Dropdown.Item onClick={() => {}}>Mark as read</Dropdown.Item>} */}
+            {props.list !== "Reading" &&
+            <Dropdown.Item onClick={() => { moveToList("reading_books") }}>Mark as reading</Dropdown.Item>}
+            {props.list !== "To read" &&
+            <Dropdown.Item onClick={() => { moveToList("toread_books") }}>Mark as to read</Dropdown.Item>}
+          </Dropdown.Menu>
+        </Dropdown>
       </Col>
     </Row>
   </>;
